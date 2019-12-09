0e390c825d1fd8c43926179408dc3a14a5daf17b
JKyte <7006511+JKyte@users.noreply.github.com>
2019-12-08 20:23:52 -0500

    Checkpoint for IntcodeComputer.

--- src/main/java/advent2019/IntcodeComputer.java
+++ src/main/java/advent2019/IntcodeComputer.java
@@ -1,5 +1,7 @@
 package advent2019;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class IntcodeComputer implements Runnable {
@@ -20,64 +22,122 @@ public class IntcodeComputer implements Runnable {
             int modes = 0;
 
             if (opcode > 99) {
-                System.out.println("We got parameter modes: " + opcode);
+//                System.out.println("We got parameter modes: " + opcode);
                 int actualOpCode = opcode % 100;
-                System.out.println("Actual op code: " + actualOpCode);
+//                System.out.println("Actual op code: " + actualOpCode);
                 modes = opcode / 100;
-                System.out.println("Remainder: " + modes);
+//                System.out.println("Remainder: " + modes);
                 opcode = actualOpCode;
-
-//                System.exit(0);
             }
 
             switch (opcode) {
 
                 case 1:
                     //  OPCODE 1 -- ADD, 3 args
-                    System.out.println("1 - add");
-                    int addArg1 = register[index + 1];
-                    int addArg2 = register[index + 2];
-                    int addDest = register[index + 3];
-                    register[addDest] = register[addArg1] + register[addArg2];
+                    System.out.println("ADD -- " + register[index] + ", " + register[index + 1] + ", " + register[index + 2]);
+                    int addArgOne = parseArgument(index, 1, modes);
+                    int addArgTwo = parseArgument(index, 2, modes);
+                    register[register[index + 3]] = addArgOne + addArgTwo;
                     index += 4;
                     continue;
 
                 case 2:
                     //  OPCODE 2 -- MULTIPLY, 3 args
-                    System.out.println("2 - multiply");
-                    System.out.println("modes: " + modes);
-                    
-                    int multArg1 = register[index + 1];
-                    int multArg2 = register[index + 2];
-                    int multDest = register[index + 3];
-                    register[multDest] = register[multArg1] * register[multArg2];
+                    System.out.println("MULT -- " + register[index] + ", " + register[index + 1] + ", " + register[index + 2]);
+                    int multArgOne = parseArgument(index, 1, modes);
+                    int multArgTwo = parseArgument(index, 2, modes);
+                    register[register[index + 3]] = multArgOne * multArgTwo;
                     index += 4;
-                    System.exit(0);
                     continue;
 
                 case 3:
                     //  OPCODE 3 -- SAVE INPUT, 1 arg
-                    System.out.println("3 - input");
-                    int saveDest = register[index + 1];
+                    System.out.println("SAVE INPUT");
                     Scanner scanner = new Scanner(System.in);
                     System.out.println("Papers, please: ");
                     int input = Integer.parseInt(scanner.nextLine());
                     scanner.close();
-                    register[saveDest] = input;
+                    register[register[index + 1]] = input;
                     index += 2;
                     continue;
 
                 case 4:
                     //  OPCODE 4 -- OUTPUT, 1 arg
-                    System.out.println("4 - output");
-                    int outIndex = register[index + 1];
-                    System.out.println("Value at index " + outIndex + ": " + register[outIndex]);
+                    System.out.println("OUTPUT -- " + register[index] + ", " + register[index + 1]);
+                    int outValue = register[register[index + 1]];
+//                    int outValue = parseArgument(index, 1, modes);
+                    System.out.println("Output: " + outValue);
+                    if (isImmediateMode(1, modes)) {
+                        System.out.println("Output (immediate): " );
+                        System.exit(0);
+                    }
+//                    else {
+//                        System.out.println("Output (position=" + outIndex + "): " + register[outIndex]);
+//                    }
                     index += 2;
+                    continue;
+
+                case 5:
+                    //  OPCODE 5 -- JUMP-IF-TRUE
+                    System.out.println("JUMP-IF-TRUE");
+                    int jumpIfTrueArgOneValue = parseArgument(index, 1, modes);
+                    int jumpIfTrueArgTwoValue = parseArgument(index, 2, modes);
+
+                    if (jumpIfTrueArgOneValue == 0) {
+                        //  No Jump if zero
+                        index += 3;
+                    } else {
+                        index = jumpIfTrueArgTwoValue;
+                    }
+                    continue;
+
+                case 6:
+                    //  OPCODE 6 -- JUMP-IF-FALSE
+                    System.out.println("JUMP-IF-FALSE");
+                    int jumpIfFalseArgOneValue = parseArgument(index, 1, modes);
+                    int jumpIfFalseArgTwoValue = parseArgument(index, 2, modes);
+                    if (jumpIfFalseArgOneValue == 0) {
+                        //  If the first parameter is zero, set the instruction point to the second arg value.
+                        index = jumpIfFalseArgTwoValue;
+                    } else {
+                        //  No Jump if non-zero
+                        index += 3;
+                    }
+                    continue;
+
+                case 7:
+                    //  OPCODE 7 -- LESS-THAN
+                    System.out.println("LESS-THAN");
+                    int lessThanArgOneValue = parseArgument(index, 1, modes);
+                    int lessThanArgTwoValue = parseArgument(index, 2, modes);
+//                    int lessThanArgThreeValue = parseArgument(index, 3, modes);
+                    int lessThanDest = register[index + 3];
+                    if (lessThanArgOneValue < lessThanArgTwoValue) {
+                        register[lessThanDest] = 1;
+                    } else {
+                        register[lessThanDest] = 0;
+                    }
+                    index += 4;
+                    continue;
+
+                case 8:
+                    //  OPCODE 8 -- EQUALS
+                    System.out.println("EQUALS -- " + register[index] + ", " + register[index + 1] + ", " + register[index + 2]);
+                    int equalsArgOneValue = parseArgument(index, 1, modes);
+                    int equalsArgTwoValue = parseArgument(index, 2, modes);
+//                    int equalsArgThreeValue = parseArgument(index, 3, modes);
+                    int equalsDestArg = register[index + 3];
+                    if (equalsArgOneValue == equalsArgTwoValue) {
+                        register[equalsDestArg] = 1;
+                    } else {
+                        register[equalsDestArg] = 0;
+                    }
+                    index += 4;
                     continue;
 
                 case 99:
                     //  OPCODE 99 -- END PROGRAM
-                    System.out.println("99 - end program");
+                    System.out.println("99 -- END PROGRAM");
                     running = false;
                     continue;
 
@@ -86,6 +146,49 @@ public class IntcodeComputer implements Runnable {
                     System.exit(0);
             }
         }
+    }
+
+    public int parseArgument(int opcodeIndex, int argIndex, int modes) {
+        if (isImmediateMode(argIndex, modes)) {
+            //  If mode is ONE then execute in IMMEDIATE MODE (value of arg is value)
+            return register[opcodeIndex + argIndex];
+        } else {
+            //  If mode is ZERO then execute in POSITION MODE (value is register[arg_value]
+            return register[register[opcodeIndex + argIndex]];
+        }
+    }
+
+    /**
+     * Position mode is mode 0
+     * Immediate mode is mode 1
+     *
+     * @param argIndex
+     * @param modes
+     * @return
+     */
+    public boolean isImmediateMode(int argIndex, int modes) {
+        if (modes == 0) {
+            return false;
+        }
+
+        if (modes > 0) {
+            //  Check for possibility of immediate mode.
+
+            String modeString = Integer.toString(modes);
+            //  If the argIndex is greater than the mode string, then assume zero -- immediate mode.
+            if (argIndex <= modeString.length()) {
+                //  Otherwise the mode string falls within our bounds.
+                modeString = StringUtils.reverse(modeString);
+                int mode = Integer.parseInt("" + modeString.charAt(argIndex - 1));
+//                System.out.println("Mode Int: " + mode);
+                if (mode == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public int valueAtIndex(int index) {
--- src/test/java/advent2019/IntcodeComputerTest.java
+++ src/test/java/advent2019/IntcodeComputerTest.java
@@ -3,6 +3,11 @@ package advent2019;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class IntcodeComputerTest {
@@ -66,8 +71,9 @@ public class IntcodeComputerTest {
     public void testAdvent2019Day05Example1() {
         //  Example. Prints out what the prompted input is.
         int[] input = {3, 0, 4, 0, 99};
-        IntcodeComputer computer = new IntcodeComputer(input);
-        computer.run();
+        String[] inputs = {"1234"};
+        String[] outputs = {"SAVE INPUT\nPapers, please: \nOUTPUT\nOutput: 1234\n99 -- END PROGRAM\n"};
+        testInputsAndOutputs(input, inputs, outputs);
     }
 
     @Test
@@ -76,5 +82,115 @@ public class IntcodeComputerTest {
         IntcodeComputer computer = new IntcodeComputer(input);
         computer.run();
         assertEquals(99, computer.valueAtIndex(4));
+    }
+
+    /**
+     * Using position mode, consider whether the input is equal to 8;
+     * output 1 (if it is) or 0 (if it is not).
+     */
+    @Ignore
+    @Test
+    public void testAdvent2019Day05Example3() {
+        int[] input = {3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8};
+        IntcodeComputer computer = new IntcodeComputer(input);
+        computer.run();
+        assertEquals(99, computer.valueAtIndex(4));
+    }
+
+    /**
+     * Using position mode, consider whether the input is less than 8;
+     * output 1 (if it is) or 0 (if it is not).
+     */
+    @Ignore
+    @Test
+    public void testAdvent2019Day05Example4() {
+        int[] input = {3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8};
+        IntcodeComputer computer = new IntcodeComputer(input);
+        computer.run();
+        assertEquals(99, computer.valueAtIndex(4));
+    }
+
+    /**
+     * Using immediate mode, consider whether the input is equal to 8;
+     * output 1 (if it is) or 0 (if it is not).
+     */
+    @Ignore
+    @Test
+    public void testAdvent2019Day05Example5() {
+        int[] input = {3, 3, 1108, -1, 8, 3, 4, 3, 99};
+        IntcodeComputer computer = new IntcodeComputer(input);
+        computer.run();
+        assertEquals(99, computer.valueAtIndex(4));
+    }
+
+    /**
+     * Using immediate mode, consider whether the input is less than 8;
+     * output 1 (if it is) or 0 (if it is not).
+     */
+    @Ignore
+    @Test
+    public void testAdvent2019Day05Example6() {
+        int[] input = {3, 3, 1107, -1, 8, 3, 4, 3, 99};
+        IntcodeComputer computer = new IntcodeComputer(input);
+        computer.run();
+        assertEquals(99, computer.valueAtIndex(4));
+    }
+
+    /**
+     * Here are some jump tests that take an input, then output 0 if the input was zero
+     * or 1 if the input was non-zero:
+     */
+    @Ignore
+    @Test
+    public void testAdvent2019Day05Example7() {
+        int[] input = {3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}; //  Position mode
+//        int[] input = {3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}; // Immediate mode
+        IntcodeComputer computer = new IntcodeComputer(input);
+        computer.run();
+//        assertEquals(99, computer.valueAtIndex(4));
+    }
+
+    @Ignore
+    @Test
+    public void testAdvent2019Day05Example8() {
+        int[] input = {3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99};
+        IntcodeComputer computer = new IntcodeComputer(input);
+        computer.run();
+//        assertEquals(99, computer.valueAtIndex(4));
+    }
+
+    private void testInputsAndOutputs(int[] program, String[] inputs, String[] outputs) {
+        for (int ii = 0; ii < inputs.length; ii++) {
+            testInputsAndOutputs(program, inputs[ii], outputs[ii]);
+        }
+    }
+
+    private void testInputsAndOutputs(int[] program, String input, String output) {
+        //  Set aside original input/output stream
+        InputStream originalSystemIn = System.in;
+        PrintStream originalSystemOut = System.out;
+        try {
+            //  Setup input stream to feed program input
+            InputStream testInput = new ByteArrayInputStream(input.getBytes());
+            System.setIn(testInput);
+
+            //  Setup output stream to capture program output
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            System.setOut(ps);
+
+            IntcodeComputer computer = new IntcodeComputer(program);
+            computer.run();
+
+            //  Capture output
+            System.out.flush();
+
+            assertEquals(output, baos.toString());
+        } finally {
+            System.setIn(originalSystemIn);
+            System.setOut(originalSystemOut);
+        }
     }
 }
